a8a8ec4e2286a4d54efcb22d79f60255492a9336
lucascatarina5-png <lucascatarina5@gmail.com>
2026-08-29 07:03:56 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vale-da-serra-v81-icons-login';
+const CACHE='vale-da-serra-v82-generated-design';
 const ASSETS=['/manifest.webmanifest','/icon-192.png','/icon-512.png','/icon-maskable-192.png','/icon-maskable-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
